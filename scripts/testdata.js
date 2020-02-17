a29ed02bf42c829064bb9b157361e65148007026
greenkey-m <info@greenkey.ru>
2020-02-17 20:10:26 +0300

    7

--- scripts/testdata.js
+++ scripts/testdata.js
@@ -1,27 +1,8 @@
-const x = 'Hello daddy!';
-console.log('hello baby@');
-
 BX.ready(function () {
 
+    const x = 'Hello daddy!';
+    console.log('hello baby@');
     console.log('Fuckme');
-
-    BX.callMethod(
-        'catalog.product.list',
-        { select:{
-                id
-            } ,
-            filter:{},
-            order:{
-                id: ASC
-            },
-            navigation: 1
-        },
-        function(result)
-        {
-            if(result.error())
-                console.error(result.error().ex);
-            else
-                console.log(result.data());
-        });
+    console.log(BX);
 
 });
