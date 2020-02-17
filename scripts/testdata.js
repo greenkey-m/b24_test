5ec783e89ee5851bdda945b696055d57c5ca1933
greenkey-m <info@greenkey.ru>
2020-02-17 17:14:41 +0300

    4

--- scripts/testdata.js
+++ scripts/testdata.js
@@ -1,2 +1,26 @@
 const x = 'Hello daddy!';
 console.log('hello baby@');
+
+
+BX24.init(function () {
+
+    BX24.callMethod(
+        'catalog.product.list',
+        { select:{
+                id
+            } ,
+            filter:{},
+            order:{
+                id: ASC
+            },
+            navigation: 1
+        },
+        function(result)
+        {
+            if(result.error())
+                console.error(result.error().ex);
+            else
+                console.log(result.data());
+        });
+
+});
